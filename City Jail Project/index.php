37203e8b0c965bfca11a2ae559748af34f2f055c
David Chang <31455973+davidchiii@users.noreply.github.com>
2023-04-29 17:56:36 -0400

    change front page

--- City Jail Project/index.php
+++ City Jail Project/index.php
@@ -30,21 +30,118 @@
 			background-color: #4CAF50;
 			color: white;
 		}
+<<<<<<< HEAD
+
+        /* Style the banner image */
+		.banner-image {
+			width: 100%;
+			height: auto;
+			display: block;
+		}
+
+
+        /* Style the card container */
+        .card-container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            margin-top: 30px;
+        }
+
+        /* Style the cards */
+        .card {
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+            max-width: 300px;
+            margin: 10px;
+            text-align: center;
+            font-family: arial;
+            background-color: #f5f5f5;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+        }
+
+        /* Style the image in the cards */
+        .card img {
+            height: 200px;
+            object-fit: cover;
+        }
+
+        /* Style the text in the cards */
+        .card p {
+            font-size: 18px;
+            margin-top: 10px;
+        }
+
+        /* Style the link in the cards */
+        .card a {
+            text-decoration: none;
+            color: #333;
+            font-size: 16px;
+            margin-top: auto;
+            padding: 10px;
+            background-color: #ddd;
+            border-radius: 5px;
+            transition: background-color 0.3s;
+        }
+
+        .card a:hover {
+            background-color: #4CAF50;
+            color: white;
+        }
+=======
+>>>>>>> 06b59cba96e206ff071bdc045da1d9f145b48b88
 	</style>
 </head>
 <body>
 	<!-- Navigation bar -->
 	<div class="navbar">
 		<a class="active" href="#">City Jail Website</a>
+<<<<<<< HEAD
+		<a href="helloworld.php">Hello World</a>
+		<a href="officers.php">Officer Lookup</a>
+		<a href="criminals.php">Criminal Lookup</a>
+	</div>
+
+    <img class="banner-image" src="static\images\4569479-landscape-clouds-city-manhattan-sunset-new-york-city.jpg" alt="Banner Image">
+=======
 		<a href="connect.php">Police Records</a>
 		<a href="officers.php">City Courts</a>
 		<a href="criminals.php">City Prison</a>
 	</div>
+>>>>>>> 06b59cba96e206ff071bdc045da1d9f145b48b88
 	<!-- Main content -->
 	<div>
 		<h1>Welcome to the City Jail Website</h1>
 		<p>Here you can find information about our jail, inmate lookup, visitation hours, and more.</p>
 		<p>Please use the navigation bar above to access other city websites.</p>
 	</div>
+<<<<<<< HEAD
+
+    <!-- Card container -->
+    <div class="card-container">
+        <!-- Card 1 -->
+        <div class="card">
+            <img src="static/images/hoa.jpg" alt="Card Image 1">
+            <p>Community Impact</p>
+            <a href="https://example.com/card-link-1">Read our About!</a>
+        </div>
+        <!-- Card 2 -->
+        <div class="card">
+            <img src="static/images/consulting.jpg" alt="Card Image 2">
+            <p>Consultations</p>
+            <a href="https://example.com/card-link-2">Schedule a visit!</a>
+        </div>
+        <!-- Card 3 -->
+        <div class="card">
+            <img src="static/images/budgeting.jpg" alt="Card Image 3">
+            <p>Services</p>
+            <a href="https://example.com/card-link-3">Contact us!</a>
+        </div>
+    </div>
+
+
+=======
+>>>>>>> 06b59cba96e206ff071bdc045da1d9f145b48b88
 </body>
 </html>
