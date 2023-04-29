9ad574b3c9060aa282f71527d46bb4085d10fb6a
Chris Olmedo <cbo238@nyu.edu>
2023-04-29 19:23:49 -0400

    editied css and php files

--- City Jail Project/index.php
+++ City Jail Project/index.php
@@ -1,122 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>City Jail Website</title>
-	<style>
-		/* Style the navigation bar */
-		.navbar {
-			background-color: #333;
-			overflow: hidden;
-		}
-
-		/* Style the links inside the navigation bar */
-		.navbar a {
-			float: left;
-			color: #f2f2f2;
-			text-align: center;
-			padding: 14px 16px;
-			text-decoration: none;
-			font-size: 17px;
-		}
-
-		/* Change the color of links on hover */
-		.navbar a:hover {
-			background-color: #ddd;
-			color: black;
-		}
-
-		/* Set an active/current link */
-		.navbar .active {
-			background-color: #4CAF50;
-			color: white;
-		}
-<<<<<<< HEAD
-
-        /* Style the banner image */
-		.banner-image {
-			width: 100%;
-			height: auto;
-			display: block;
-		}
-
-
-        /* Style the card container */
-        .card-container {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-            margin-top: 30px;
-        }
-
-        /* Style the cards */
-        .card {
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-            max-width: 300px;
-            margin: 10px;
-            text-align: center;
-            font-family: arial;
-            background-color: #f5f5f5;
-            padding: 20px;
-            display: flex;
-            flex-direction: column;
-        }
-
-        /* Style the image in the cards */
-        .card img {
-            height: 200px;
-            object-fit: cover;
-        }
-
-        /* Style the text in the cards */
-        .card p {
-            font-size: 18px;
-            margin-top: 10px;
-        }
-
-        /* Style the link in the cards */
-        .card a {
-            text-decoration: none;
-            color: #333;
-            font-size: 16px;
-            margin-top: auto;
-            padding: 10px;
-            background-color: #ddd;
-            border-radius: 5px;
-            transition: background-color 0.3s;
-        }
-
-        .card a:hover {
-            background-color: #4CAF50;
-            color: white;
-        }
-=======
->>>>>>> 06b59cba96e206ff071bdc045da1d9f145b48b88
-	</style>
+    <link rel="stylesheet" type="text/css" href="static/css/header-style.css">
+    <link rel="stylesheet" type="text/css" href="static/css/index.css">
 </head>
 <body>
 	<!-- Navigation bar -->
 	<div class="navbar">
 		<a class="active" href="#">City Jail Website</a>
-<<<<<<< HEAD
-		<a href="helloworld.php">Hello World</a>
 		<a href="officers.php">Officer Lookup</a>
 		<a href="criminals.php">Criminal Lookup</a>
 	</div>
 
     <img class="banner-image" src="static\images\4569479-landscape-clouds-city-manhattan-sunset-new-york-city.jpg" alt="Banner Image">
-=======
 		<a href="connect.php">Police Records</a>
 		<a href="officers.php">City Courts</a>
 		<a href="criminals.php">City Prison</a>
 	</div>
->>>>>>> 06b59cba96e206ff071bdc045da1d9f145b48b88
 	<!-- Main content -->
 	<div>
 		<h1>Welcome to the City Jail Website</h1>
 		<p>Here you can find information about our jail, inmate lookup, visitation hours, and more.</p>
 		<p>Please use the navigation bar above to access other city websites.</p>
 	</div>
-<<<<<<< HEAD
 
     <!-- Card container -->
     <div class="card-container">
@@ -139,9 +48,5 @@
             <a href="https://example.com/card-link-3">Contact us!</a>
         </div>
     </div>
-
-
-=======
->>>>>>> 06b59cba96e206ff071bdc045da1d9f145b48b88
 </body>
 </html>
--- City Jail Project/officers.php
+++ City Jail Project/officers.php
@@ -1,1 +1,13 @@
-<!-- code to print all officers -->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Officer Lookup</title>
+	<link rel="stylesheet" type="text/css" href="static/css/header-style.css">
+    <link rel="stylesheet" type="text/css" href="static/css/officer.css">
+</head>
+<body>
+
+</body>
+</html>
