ab87e3ab45a780323299f85df3652b24e8f74aa6
davidchiii <changdavid52@gmail.com>
2023-04-30 14:56:13 -0400

    start criminal payments

--- City Jail Project/payment.php
+++ City Jail Project/payment.php
@@ -9,14 +9,45 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 
 // Check if the user is an admin
 $is_admin = isset($_SESSION["is_admin"]) && $_SESSION["is_admin"] == "admin";
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "jail";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_SESSION['criminal_id'])) {
+  $criminal_id = $_SESSION['criminal_id'];
+  echo "Criminal ID: " . $criminal_id;
+} else {
+  echo "No criminal ID found in session.";
+}
+
+// // Get the crimes the criminal has committed from the crimes table
+// $sql = "SELECT crime_id FROM crimes WHERE criminal_id=$criminal_id";
+
+// $result = $conn->query($sql);
+// while($row = $result->fetch_assoc()) {
+//   $crime_id = $row["crime_id"];
+//   $sql = "SELECT charge FROM crime_charges WHERE crime_id=$crime_id";
+//   $charge_result = $conn->query($sql);
+//   $charge_row = $charge_result->fetch_assoc();
+//   $total_charge += $charge_row["charge"];
+
 ?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Officer Lookup</title>
+    <title>Payment</title>
     <link rel="stylesheet" type="text/css" href="static/css/header-style.css">
     <link rel="stylesheet" type="text/css" href="static/css/officer.css">
 </head>
@@ -39,5 +70,8 @@ $is_admin = isset($_SESSION["is_admin"]) && $_SESSION["is_admin"] == "admin";
             <a class="login" href="login.php">Log In</a>
         <?php endif; ?>
     </div>
+    
+
+
 </body>
 </html>
